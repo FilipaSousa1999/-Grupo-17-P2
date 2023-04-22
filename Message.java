266e5b93a9cd405941a49819c348351a7d51c2d4
FilipaSousa1999 <2043718@student.uma.pt>
2023-04-23 00:53:55 +0100

    Projeto no git pronto a começar

--- Message.java
+++ Message.java
@@ -7,14 +7,21 @@ public class Message implements Serializable {
 
     private final byte[] message;
 
+
+    private final byte[] mac;
+
     /**
-     * Constructs a Message object by specifying the message bytes that will be sent to the server.
+     * Constructs a Message object by specifying the message bytes that will be sent to the server and the message
+     * authentication code (MAC).
      *
      * @param message the message that is sent to the server
+     * @param mac     the message authentication code (MAC)
      */
-    public Message ( byte[] message ) {
+    public Message ( byte[] message , byte[] mac ) {
         this.message = message;
+        this.mac = mac;
     }
+
 
     /**
      * Gets the message string.
@@ -22,6 +29,10 @@ public class Message implements Serializable {
      * @return the message string
      */
     public byte[] getMessage ( ) {
+
         return message;
+    }
+    public byte[] getMac ( ) {
+        return mac;
     }
 }
